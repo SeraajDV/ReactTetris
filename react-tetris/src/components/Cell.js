70a4043820abd4794acfab95e8231d6de62bed75
SeraajDV <SeraajDV@users.noreply.github.com>
2019-09-03 10:28:28 +0200

    final commit

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -3,7 +3,9 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={'L'} color={TETROMINOS['L'].color} />
-)
+  <StyledCell type={type} color={TETROMINOS[type].color}>
+    {console.log('rerender cell')}
+  </StyledCell>
+);
 
-export default Cell;
+export default React.memo(Cell);
